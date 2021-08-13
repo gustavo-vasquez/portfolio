2c3f82399a94fef35cd755f4fd3ce8ffed48658d
Gustavo Vasquez <vasquez.mgustavo@gmail.com>
2021-08-13 13:24:21 -0300

    General -> tipografía y tamaño del texto base, color de enlaces. Sombras y tipografía en baldosas de proyectos. Carousel -> optimizada lógica, las imágenes se generan a partir de las vistas previas.

--- js/carousel.js
+++ js/carousel.js
@@ -1,40 +1,56 @@
 (function() {
-	const carouselImages = document.querySelectorAll(".carousel-grid > img");
+	const carouselPreviews = document.querySelectorAll(".carousel-grid > img");
+	const carouselTitle = document.querySelector('.carousel-container > .card-header') || document.querySelector('.carousel-container > .card-footer');
+	const carouselGallery = document.querySelector('.carousel-container > .carousel-gallery');
+
 	let gallery = {
 		currentImage: 0,
 		direction: { right: 1, left: 0 },
 		list: []
 	};
 	
-	carouselImages.forEach(image => {
-		gallery.list.push({ image: image.src, text: image.alt, title: image.dataset.title });
-	});
-	
-	document.querySelector('.carousel-arrow.right').addEventListener('click', function() { carouselGoTo(gallery.direction.right, this) });
-	document.querySelector('.carousel-arrow.left').addEventListener('click', function() { carouselGoTo(gallery.direction.left, this) });
-	
-	carouselImages.forEach(element => {
-		element.addEventListener('click', function() {
+	carouselPreviews.forEach((image, index) => {
+		gallery.list.push({ image: image.getAttribute("src"), text: image.alt, title: image.dataset.title });
+
+		image.addEventListener('click', function() {
 			let thisContext = this;
 			
-			if(thisContext.src !== gallery.list[gallery.currentImage].image) {
-				gallery.currentImage = gallery.list.findIndex(element => element.image === thisContext.src);
+			if(thisContext.getAttribute("src") !== gallery.list[gallery.currentImage].image) {
+				gallery.currentImage = gallery.list.findIndex(element => element.image === thisContext.getAttribute("src"));
 				updateCarousel();
 			}
 		});
+
+		let img = document.createElement("IMG");
+		img.src = gallery.list[index].image;
+		img.alt = image.alt;
+
+		if(index !== gallery.currentImage)
+			img.classList.add('img-fluid','d-none');
+		else {
+			carouselTitle.innerHTML = image.dataset.title;
+			img.classList.add('img-fluid');
+		}
+
+		carouselGallery.append(img);
 	});
 	
-	function carouselGoTo(direction, thisContext) {
-		let currentImage = thisContext.parentElement.querySelector('img');
+	document.querySelector('.carousel-arrow.right').addEventListener('click', function() { carouselGoTo(gallery.direction.right) });
+	document.querySelector('.carousel-arrow.left').addEventListener('click', function() { carouselGoTo(gallery.direction.left) });
+	
+	function carouselGoTo(direction) {
+		let slideDirectionClass;
 		
 		switch(direction) {
 			case gallery.direction.right:
+				slideDirectionClass = "slide-to-left";
 				if(gallery.currentImage < gallery.list.length - 1)
 					gallery.currentImage = gallery.currentImage + 1;
 				else
 					gallery.currentImage = 0;
 				break;
 			case gallery.direction.left:
+				slideDirectionClass = "slide-to-right";
 				if(gallery.currentImage > 0)
 					gallery.currentImage = gallery.currentImage - 1;
 				else
@@ -44,29 +60,27 @@
 				break;
 		}
 		
-		updateCarousel();
+		updateCarousel(slideDirectionClass);
 		
 	}
 	
-	function updateCarousel() {
-		const carouselGallery = document.querySelector('.carousel-gallery');
-		
-		for(children of carouselGallery.children) {
-			if(children.nodeName === 'IMG') {
-				let clone = children.cloneNode(true);
-				clone.src = gallery.list[gallery.currentImage].image;
-				clone.alt = gallery.list[gallery.currentImage].text;
-				children.remove();
-				carouselGallery.appendChild(clone);
+	function updateCarousel(slideDirectionClass) {
+		carouselTitle.innerHTML = gallery.list[gallery.currentImage].title;
+
+		carouselGallery.querySelectorAll('img').forEach((image, index) => {
+			image.classList.remove("slide-to-left", "slide-to-right");
+
+			if(index === gallery.currentImage) {
+				if(slideDirectionClass !== undefined)
+					image.classList.add(slideDirectionClass);
+				
+				image.classList.remove("d-none");
 			}
-		}
+			else
+				image.classList.add("d-none");
+		});
 		
-		for(children of carouselGallery.parentElement.children) {
-			if(children.classList.contains('card-footer'))
-				children.innerHTML = gallery.list[gallery.currentImage].title;
-		}
-		
-		carouselImages.forEach((image, index) => {
+		carouselPreviews.forEach((image, index) => {
 			if(index === gallery.currentImage)
 				image.classList.add("active");
 			else
